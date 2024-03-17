75a5f2d1f9deed81d83f74c853644ebd9078574a
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-03-17 07:59:19 -0300

    reiniciar el estado de calculadora, eliminar un numero del currentOperand y actualizar el display

--- math-calculator/js/index.js
+++ math-calculator/js/index.js
@@ -17,3 +17,57 @@ for (let link of links) {
     bg.style.width = `${link.offsetWidth}px`;
   });
 }
+
+/* calculator logic */
+const display = document.querySelector(".calculator_display");
+const previousOperandTextElement = document.querySelector(".previous_operand");
+const currentOperandTextElement = document.querySelector(".current_operand");
+const buttons = document.querySelectorAll(".calculator_button");
+
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.clear();
+  }
+
+  appendNumber(number) {
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+    this.updateDisplay();
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.updateDisplay();
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.slice(0, -1);
+    this.updateDisplay();
+  }
+
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = this.previousOperand;
+  }
+}
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerText.match(/[0-9]/)) {
+      calculator.appendNumber(e.target.innerText);
+    }
+    if (e.target.innerText === "ac") {
+      calculator.clear();
+    }
+    if (e.target.innerText === "del") {
+      calculator.delete();
+    }
+  });
+});
